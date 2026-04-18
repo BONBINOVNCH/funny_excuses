9168f930617d8484ce9bb40751ac3c0e04329883
Max <zmax1301@gmail.com>
2026-04-18 13:52:06 +0200

    small_schablon

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.json());
+
+app.get("/exucesses", (req, res) => {
+    res.send("Hello World 1!");
+});
+
+app.get("/situations", (req, res) => {
+    res.send("Hello World 2!");
+});
+
+app.get("/all-exucess_and_situations", (req, res) => {
+    res.send("Hello World 3!");
+});
+
+app.post("/exucesses/add", (req, res) => {
+    res.send("Hello World 4!");
 });
 
 app.listen(port, () => {
